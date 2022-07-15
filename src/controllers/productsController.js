6f9ef24398e45481775ad23d2f19b2756c2d229b
Cami0807 <camilacaracciolo@hotmail.com>
2022-07-15 17:09:28 -0300

    .

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -10,11 +10,11 @@ const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 const controller = {
 
 	carrito: (req, res) => {
-        return res.render('/productos/carrito');
+        return res.render('./productos/carrito');
     },
 
     compras: (req, res) => {
-        return res.render('/productos/compras');
+        return res.render('./productos/compras');
     },
 
 	// Detail - Detail from one product
