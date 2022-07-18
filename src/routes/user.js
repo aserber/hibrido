65536d09edaf2f69a814f3e9dfe9413acab4becc
Cami0807 <camilacaracciolo@hotmail.com>
2022-07-18 19:09:11 -0300

    ..

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
-const uploadFile = require ("../middlewares/multerMiddleware");
-const validations = require ("../middlewares/validateRegisterMiddleware");
+//const uploadFile = require ("../middlewares/multerMiddleware");
+//const validations = require ("../middlewares/validateRegisterMiddleware");
 
 
 
 router.get('/registro', userController.register); //u
 
-router.post('/registro', uploadFile.single("avatar"), validations , userController.processRegister);
+//router.post('/registro', uploadFile.single("avatar"), validations , userController.processRegister);
 
 router.get('/login', userController.login); //u
 
