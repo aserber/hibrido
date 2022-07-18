5743b1e302f537ed1b528b378daf65b2d5c160bc
Cami0807 <camilacaracciolo@hotmail.com>
2022-07-17 22:00:13 -0300

    probando users

--- src/controllers/usercontroller.js
+++ src/controllers/usercontroller.js
@@ -2,7 +2,7 @@
 //const fs = require('fs');
 //const bcrypt = require('bcryptjs');
 //const multer = require('multer');
-//const { validationResult } = require('express-validator');
+const { validationResult } = require('express-validator');
 
 const controller = {
     register: (req, res) => {
@@ -11,6 +11,20 @@ const controller = {
     login: (req, res) => {
         return res.render('usuario/login');
     },
+    profile: (req,res) => {
+        return res.render ("usuario/login");
+    },
+    processRegister: (req,res) => {
+        const resultValidation = validationResult(req);
+
+        if(resultValidation.errors.length > 0) {
+           return res.render ("usuario/registro", {
+            errors: resultValidation.mapped(),
+              oldData: req.body
+            });
+        }
+            return res.send ("ok");
+     }
 
 }
 
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const uploadFile = require ("../middlewares/multerMiddleware");
+const validations = require ("../middlewares/validateRegisterMiddleware");
+
+
 
 router.get('/registro', userController.register); //u
 
+router.post('/registro', uploadFile.single("avatar"), validations , userController.processRegister);
+
 router.get('/login', userController.login); //u
 
+router.get ("../profile/:userId", userController.profile);
 
 module.exports = router;
