567db3313341a19ed4aef99c1d23878ecd5d0c08
Cami0807 <camilacaracciolo@hotmail.com>
2022-07-25 22:58:06 -0300

    productos ok

--- src/controllers/usercontroller.js
+++ src/controllers/usercontroller.js
@@ -1,9 +1,10 @@
-//const path = require('path');
-//const fs = require('fs');
-//const bcrypt = require('bcryptjs');
+const path = require('path');
+const fs = require('fs');
+const bcrypt = require('bcrypt');
 //const multer = require('multer');
 const { validationResult } = require('express-validator');
 const User = require ("../database/models/User");
+const userFilePath = path.join(__dirname, '../data/usersBase.json');
 
 const controller = {
     register: (req, res) => {
@@ -15,23 +16,40 @@ const controller = {
     profile: (req,res) => {
         return res.render ("usuario/login");
     },
-    processRegister: (req,res) => {
-        const resultValidation = validationResult(req);
+    	save: (req, res) => {
 
-        if(resultValidation.errors.length > 0) {
-           return res.render ("usuario/registro", {
-            errors: resultValidation.mapped(),
-              oldData: req.body
-            });
-        }   
+		const user = JSON.parse(fs.readFileSync(userFilePath, 'utf-8'));
+		let newUser = {
+			id: user[user.length - 1].id + 1,
+			name: req.body.name,
+			last_name: req.body.last_name,
+			email: req.body.email,
+            password: bcrypt.hashSync(req.body.password,10),
+			category: 1,
+		}
+		console.log(newUser)
+		user.push(newUser);
+		fs.writeFileSync(userFilePath, JSON.stringify(user, null, ' '));
+		res.redirect('/');
 
-        let userToCreate = {
-            ...req.body,
-            avatar: req
-        }
-            User.create(req.body);
-            return res.send ("ok");
-     }
+	},
+    //processRegister: (req,res) => {
+      //  const resultValidation = validationResult(req);
+
+        //if(resultValidation.errors.length > 0) {
+          // return res.render ("usuario/registro", {
+            //errors: resultValidation.mapped(),
+              //oldData: req.body
+           // });
+        //}   
+
+        //let userToCreate = {
+         //   ...req.body,
+          //  avatar: req
+       // }
+         //   User.create(req.body);
+           // return res.send ("ok");
+     //}
 
 }
 
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,18 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
-//const uploadFile = require ("../middlewares/multerMiddleware");
 const validator = require ("../middlewares/validateRegisterMiddlewares");
 
 
 
 router.get('/registro', userController.register); //u
-
-// uploadFile.single("avatar"),
-router.post('/registro', validator.register , userController.processRegister);
-
+router.post('/registro', validator.register , userController.save);
 router.get('/login', userController.login); //u
-
-router.get ("../profile/:userId", userController.profile);
+router.get ('../profile/:userId', userController.profile);
 
 module.exports = router;
